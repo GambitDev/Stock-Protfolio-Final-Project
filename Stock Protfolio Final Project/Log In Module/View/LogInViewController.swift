caa89da000c9a0642546e90d8d79089bf7781040
GambitDev <gambitdg@gmail.com>
2021-01-02 21:16:55 +0200

    user can now register

--- Stock Protfolio Final Project/Log In Module/View/LogInViewController.swift
+++ Stock Protfolio Final Project/Log In Module/View/LogInViewController.swift
@@ -17,12 +17,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var registerButton: UIButton!
     
     //MARK: - Properties
+    private let presenter = LogInScreenPresenter()
 
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configurePage()
+    }
+    
+    //MARK: - Actions
+    @IBAction func logInButtonClicked(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerButtonClicked(_ sender: UIButton) {
+        showRegisterAlertController()
     }
     
     //MARK: - Methods
@@ -33,6 +42,31 @@ class ViewController: UIViewController {
     private func setButtonsStyle() {
         logInButton.layer.cornerRadius = logInButton.bounds.height / 2
         registerButton.layer.cornerRadius = registerButton.bounds.height / 2
+    }
+    
+    private func showRegisterAlertController() {
+        
+        let alertController = UIAlertController(title: "Register A New Account", message: nil, preferredStyle: .alert)
+        
+        alertController.addTextField() { textField in
+            textField.placeholder = "Username"
+        }
+        
+        alertController.addTextField() { textField in
+            textField.placeholder = "Password"
+            textField.isSecureTextEntry = true
+        }
+        
+        alertController.addAction(UIAlertAction(title: "Register An Account", style: .default) { [weak self] action in
+            guard let username = alertController.textFields?[0].text, let password = alertController.textFields?[1].text else {
+                return
+            }
+            self?.presenter.registerNewUser(userName: username, password: password)
+        })
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     //MARK: System Methods
