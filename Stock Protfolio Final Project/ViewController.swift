ab10c45373728b6f590dbeda2b1f1d43836e2591
GambitDev <gambitdg@gmail.com>
2021-01-02 19:30:52 +0200

    Initial Commit

---
+++ Stock Protfolio Final Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stock Protfolio Final Project
+//
+//  Created by Dan Gavrielov on 02/01/2021.
+//  Copyright © 2021 Dan Gavrielov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
